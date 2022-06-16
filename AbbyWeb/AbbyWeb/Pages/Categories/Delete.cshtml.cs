378b3c1b1aef7ec6847dbcd7e77fa35b08b51921
arnab031 <51916075+arnab031@users.noreply.github.com>
2022-06-17 03:19:27 +0530

    Add category CRUD Operation

---
+++ AbbyWeb/AbbyWeb/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AbbyWeb.Data;
+using AbbyWeb.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Categories
+{
+    [BindProperties]
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        
+        public Category Category { get; set; }
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int id)
+        {
+            Category = _db.Category.FirstOrDefault(u=>u.Id==id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var categoryFromDb = _db.Category.Find(Category.Id);
+            if (categoryFromDb != null)
+            {
+                _db.Category.Remove(categoryFromDb);
+                await _db.SaveChangesAsync();
+                TempData["success"] = "Category deleted successfully";
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
---
+++ AbbyWeb/AbbyWeb/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+builder.Services.AddDbContext<AbbyWeb.Data.ApplicationDbContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+
